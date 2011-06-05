a825173d8e46611bd9472b3e9da5a17cdc82c320
Dylan <dylanbathurst@Dylans-MacBook-Air.local>
2011-06-05 10:46:03 -0700

    mu implemented

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var sys = require('sys'),
     http = require('http'),
     path = require('path'),
     url = require('url'),
-    connect = require('connect'),
+    mu = require('../Mu/lib/mu'),
     events = require('events').EventEmitter,
     couchdb = require('./lib/couchdb'),
     fs = require('fs');
@@ -13,20 +13,24 @@ couchdb.setup({
 });
 
 
+
 http.createServer(function (req, res) {
 
   var uri = url.parse(req.url).pathname;
 
   switch (uri) {
     case '/':
-      var fileEmitter = new events(),
-          buffer = '';
 
-      fileEmitter.on('end', function () {
+      var fileEmitter = new events(),
+          view = {posts: []};
+
+
+      mu.fs('./templates/layout.html', function (err, data) {
+        if (err) throw err;
 
         var handler = function (posts) {
           var couchRes = JSON.parse(posts), i, len, buff = '';
-        
+
           len = couchRes.rows.length;
 
           for (var i = 0; i < len; i++) {
@@ -34,26 +38,31 @@ http.createServer(function (req, res) {
                 title = post.title,
                 body = post.post;
 
-            buff += '<h2>' + title + '</h2>' + '<article>' + body + '</article>'  
-          } 
+            view.posts.push({title: title, post: body});
+          }
 
-          res.writeHead(200, {'Content-Type': 'text/html'});
-          res.write(buffer + buff);
-          res.end();
+          var numErrors = 0,
+              buffer = '';
+
+          mu.compileText('layout.html', data);
+
+          var stream = mu.render('layout.html', view)
+            .on('data', function (chunks) { 
+              buffer += chunks;
+            })
+            .on('end',  function () {
+              res.writeHead(200, {'Content-Type': 'text/html'});
+              res.write(buffer);
+              res.end();
+            })
+            .on('error', function (err) { numErrors++; });
+
         };
 
         couchdb.getView('/_design/posts/_view/posts', handler);
 
       });
 
-      fs.readFile('./templates/layout.html', function (err, data) {
-        if (err) {
-          fileEmitter.emit('error');
-        }
-  
-        buffer += data;
-        fileEmitter.emit('end');
-      });
 
     break;
     case '/styles.css':
