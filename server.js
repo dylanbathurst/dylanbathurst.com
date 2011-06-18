7c9e9aa9651568505ec81c9de2052a87d590d5ba
dylanbathurst <dylanbathurst@gmail.com>
2011-06-18 16:29:37 -0700

    Revert "changed check for dev or prod code"
    This reverts commit 120a85f3678f556cd1a34eb6bd9cfe6f2ecc1f69.
    
    Conflicts:
    
    	server.js

--- server.js
+++ server.js
@@ -3,8 +3,17 @@ var http = require('http'),
     url = require('url'),
     pages = require('./lib/pages');
 
-console.log(process.env.PORT);
-var serverPort = process.env.PORT || 8080;
+var cache = {},
+    couchHost,
+    serverPort;
+
+if (process.env.DEV) {
+  couchHost = '127.0.0.1';
+  serverPort = 8080;
+} else {
+  couchHost = 'dylan.couchone.com';
+  serverPort = 80;
+}
 
 http.createServer(function (req, res) {
 
@@ -13,7 +22,61 @@ http.createServer(function (req, res) {
   switch (uri) {
     case '/':
 
-      pages.home(req, res);
+      if ('homepage' in cache) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(cache.homepage);
+        res.end();
+      } else {
+
+        couchdb.setup({
+          db: 'dylanbathurstcom',
+          host: couchHost
+        });
+
+        var fileEmitter = new events(),
+            view = {posts: []};
+
+
+        mu.fs('./templates/layout.html', function (err, data) {
+          if (err) throw err;
+
+          var handler = function (posts) {
+            var couchRes = JSON.parse(posts), i, len, buff = '';
+
+            len = couchRes.rows.length;
+
+            for (var i = 0; i < len; i++) {
+              var post = couchRes.rows[i].value,
+                  title = post.title,
+                  body = post.post;
+
+              view.posts.push({title: title, post: body});
+            }
+
+            var numErrors = 0,
+                buffer = '';
+
+            mu.compileText('layout.html', data);
+
+            var stream = mu.render('layout.html', view)
+              .on('data', function (chunks) { 
+                buffer += chunks;
+              })
+              .on('end',  function () {
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.write(buffer);
+                res.end();
+                cache['homepage'] = buffer;
+              })
+              .on('error', function (err) { numErrors++; });
+
+          };
+
+          couchdb.getView('/_design/posts/_view/posts', handler);
+
+        });
+
+      }
 
     break;
     case '/clearcache':
