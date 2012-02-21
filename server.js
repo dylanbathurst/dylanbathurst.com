069fcf4aaffecf2959965355b2e1f83a20290c03
dylanbathurst <dylanbathurst@gmail.com>
2012-02-21 01:57:36 -0800

    adding apime integrationo

--- server.js
+++ server.js
@@ -1,105 +1,31 @@
-var http = require('http'),
-    path = require('path'),
-    url = require('url'),
-    pages = require('./lib/pages'),
-    mu = require('mu'),
-    events = require('events').EventEmitter,
-    couchdb = require('./lib/couchdb'),
-    fs = require('fs');
+var http = require('http');
+var express = require('express');
 
-var cache = {},
-    couchHost,
-    serverPort;
+var app = express.createServer();
 
-if (process.env.DEV) {
-  couchHost = '127.0.0.1';
-  serverPort = 8080;
-} else {
-  couchHost = 'dylan.couchone.com';
-  serverPort = 80;
-}
+app.get('/', function (req, res, next) {
+  res.writeHead(200, {'Content-Type': 'application/json'});
 
-http.createServer(function (req, res) {
-
-  var uri = url.parse(req.url).pathname;
-
-  switch (uri) {
-    case '/':
-
-      if ('homepage' in cache) {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(cache.homepage);
-        res.end();
-      } else {
-
-        couchdb.setup({
-          db: 'dylanbathurstcom',
-          host: couchHost
-        });
-
-        var fileEmitter = new events(),
-            view = {posts: []};
+  var options = {
+    host: 'blazing-galaxy-2505.herokuapp.com',
+    path: '/dylanbathurst',
+    method: 'GET'
+  };
 
 
-        mu.fs('./templates/layout.html', function (err, data) {
-          if (err) throw err;
+  var req = http.request(options, function (response) {
+    response.on('data', function (chunk) {
+      res.write(chunk);
+    });
 
-          var handler = function (posts) {
-            var couchRes = JSON.parse(posts), i, len, buff = '';
+    response.on('end', function () {
+      res.end();
+    });
+  });
 
-            len = couchRes.rows.length;
+  req.on('error', function (err) {});
 
-            for (var i = 0; i < len; i++) {
-              var post = couchRes.rows[i].value,
-                  title = post.title,
-                  body = post.post;
+  req.end();
+});
 
-              view.posts.push({title: title, post: body});
-            }
-
-            var numErrors = 0,
-                buffer = '';
-
-            mu.compileText('layout.html', data);
-
-            var stream = mu.render('layout.html', view)
-              .on('data', function (chunks) { 
-                buffer += chunks;
-              })
-              .on('end',  function () {
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.write(buffer);
-                res.end();
-                cache['homepage'] = buffer;
-              })
-              .on('error', function (err) { numErrors++; });
-
-          };
-
-          couchdb.getView('/_design/posts/_view/posts', handler);
-
-        });
-
-      }
-
-    break;
-    case '/clearcache':
-
-      pages.clearcache(req, res);
-
-    break;
-    case '/styles.css':
-
-      pages.styles(req, res);
-
-    break;
-    default:
-
-      pages.error(req, res);
-
-    break;
-  }
-
-
-}).listen(serverPort);
-
+app.listen(80);
